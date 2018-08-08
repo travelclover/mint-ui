d1e3f92e1aeba6043708e68af94b1df7bcd6e490
dengdong <853315284@qq.com>
2018-08-08 23:31:31 +0800

    引入‘mint-ui’框架

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,21 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  mounted() {
+    this.$nextTick(function() {
+      this.$toast({
+        message: "提示",
+        position: "bottom"
+      });
+    });
+  }
+};
+</script>
