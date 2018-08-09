0bce134193977c973e22a50be85074de8da1f0f6
dengdong <853315284@qq.com>
2018-08-09 14:21:34 +0800

    main.js中加入‘plusReady’事件监测

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Mint from "mint-ui";
 import "mint-ui/lib/style.css";
 import router from "./router";
 import store from "./store";
+import { plusReady } from "@/utils/plusReady.js";
 
 Vue.use(Mint);
 Vue.config.productionTip = false;
@@ -11,5 +12,11 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    plusReady(this.plusReady);
+  },
+  methods: {
+    plusReady() {}
+  }
 }).$mount("#app");
---
+++ src/utils/plusReady.js
@@ -0,0 +1,7 @@
+export const plusReady = fn => {
+  if (window.plus) {
+    setTimeout(fn, 0);
+  } else {
+    document.addEventListener("plusready", fn, false);
+  }
+};
